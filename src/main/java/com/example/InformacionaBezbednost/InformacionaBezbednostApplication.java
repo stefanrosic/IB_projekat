512e0028952a4a2bcb74db1f4a3b600cb09d8123
Stefan Rosic <stefanrosic2@gmail.com>
2019-04-30 13:09:51 +0200

    Project moved from gitlab to github

---
+++ src/main/java/com/example/InformacionaBezbednost/InformacionaBezbednostApplication.java
@@ -0,0 +1,13 @@
+package com.example.InformacionaBezbednost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacionaBezbednostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacionaBezbednostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InformacionaBezbednost/InformacionaBezbednostApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.InformacionaBezbednost;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InformacionaBezbednostApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
