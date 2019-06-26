5a9f874708187e1bd6fce5cb815572d63c243157
Stefan Rosic <stefanrosic2@gmail.com>
2019-06-26 22:03:28 +0200

    Small change

--- src/main/java/com/bezbednost/controller/UploadDataController.java
+++ src/main/java/com/bezbednost/controller/UploadDataController.java
@@ -1,4 +1,4 @@
-package com.example.InformacionaBezbednost;
+package com.bezbednost.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
