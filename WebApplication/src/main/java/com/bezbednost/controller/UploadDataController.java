c325cabb2b20dd6b0aa6d6248af7b8109a580b7a
Stefan Rosic <stefanrosic2@gmail.com>
2019-06-30 08:23:39 +0200

    Encryption of zip and png completed

--- WebApplication/src/main/java/com/bezbednost/controller/UploadDataController.java
+++ WebApplication/src/main/java/com/bezbednost/controller/UploadDataController.java
@@ -6,10 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 @RestController
 public class UploadDataController {
@@ -17,27 +31,74 @@ public class UploadDataController {
 	private static String UPLOAD_DIRECTORY = "./upload_directory/";
 	
 	@PostMapping("/upload")
-	public String fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
+	public String fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IOException
 	{
-        if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-            return "redirect:uploadStatus";
-        }
 
-        try {
+		File output= new File(UPLOAD_DIRECTORY, file.getOriginalFilename());
+		FileOutputStream outStream = new FileOutputStream(output);
+		encryptAndClose(outStream, file);   
+		
+		
+//		File input= new File(UPLOAD_DIRECTORY, file.getOriginalFilename());
+//		FileInputStream inStream = new FileInputStream(input);
+//		decryptAndClose(inStream, outStream);
 
-            // Get the file and save it somewhere
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOAD_DIRECTORY + file.getOriginalFilename());
-            Files.write(path, bytes);
-
-            redirectAttributes.addFlashAttribute("message", 
-                        "You successfully uploaded '" + file.getOriginalFilename() + "'");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return "redirect:/uploadStatus";
+        return "USPESAN UPLOAD I ZIP ENKRIPTOVAN!";
 	}
+	
+	public static void encryptAndClose(FileOutputStream fos, MultipartFile file) 
+	        throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+
+	    // Length is 16 byte
+	    SecretKeySpec sks = new SecretKeySpec("1234567890123456".getBytes(), "AES");
+	    // Create cipher
+	    Cipher cipher = Cipher.getInstance("AES");
+	    cipher.init(Cipher.ENCRYPT_MODE, sks);      
+
+	    // Wrap the output stream for encoding
+	    CipherOutputStream cos = new CipherOutputStream(fos, cipher);       
+
+	    //wrap output with buffer stream
+	    BufferedOutputStream bos = new BufferedOutputStream(cos);     
+
+	    //wrap input with buffer stream
+	    BufferedInputStream bis = new BufferedInputStream(file.getInputStream()); 
+
+	    // Write bytes
+	    int b;
+	    byte[] d = new byte[8];
+	    while((b = bis.read(d)) != -1) {
+	        bos.write(d, 0, b);
+	    }
+	    // Flush and close streams.
+	    bos.flush();
+	    //bos.close();
+	    //bis.close();
+	}
+	
+//	public static void decryptAndClose(FileInputStream fis, FileOutputStream fos) 
+//	        throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+//
+//	    SecretKeySpec sks = new SecretKeySpec("1234567890123456".getBytes(), "AES");
+//	    Cipher cipher = Cipher.getInstance("AES");
+//	    cipher.init(Cipher.DECRYPT_MODE, sks);
+//
+//	    CipherInputStream cis = new CipherInputStream(fis, cipher);
+//
+//	    //wrap input with buffer stream
+//	    BufferedInputStream bis = new BufferedInputStream(cis); 
+//
+//	    //wrap output with buffer stream
+//	    BufferedOutputStream bos = new BufferedOutputStream(fos);       
+//
+//	    int b;
+//	    byte[] d = new byte[8];
+//	    while((b = bis.read(d)) != -1) {
+//	        bos.write(d, 0, b);
+//	    }
+//	    bos.flush();
+//	    bos.close();
+//	    bis.close();
+//	}
 
 }
